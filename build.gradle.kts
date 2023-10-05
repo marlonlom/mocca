84ad9c453a36cab362f52acf82221f203aab740a
Marlon López <malm.gm@gmail.com>
2023-10-05 12:08:58 -0500

    Added root project configs

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2023 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+@file:Suppress("DSL_SCOPE_VIOLATION")
+
+buildscript {
+  repositories {
+    google()
+    mavenCentral()
+  }
+  dependencies {
+    classpath(libs.google.oss.licenses.plugin) {
+      exclude(group = "com.google.protobuf")
+    }
+  }
+}
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.kotlin.jvm) apply false
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2023 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+pluginManagement {
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
+rootProject.name = "Mocca"
+include(":apps:mobile_app")
+include(":libraries:calculator")
