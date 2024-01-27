30f9b03909bea5f8c924710957cb600b1a84af8f
Marlon López <malm.gm@gmail.com>
2024-01-26 19:50:10 -0500

    feat(wearos-app): Updated launcher icon and theme colors

--- apps/wearos-app/src/main/kotlin/dev/marlonlom/apps/mocca/wearos/presentation/theme/Color.kt
+++ apps/wearos-app/src/main/kotlin/dev/marlonlom/apps/mocca/wearos/presentation/theme/Color.kt
@@ -15,56 +15,42 @@ import androidx.wear.compose.material.Colors
  */
 internal object BrandColors {
 
-  /** Brand color value: Cape Honey. */
-  private val capeHoney = Color(0xFFFFDEA3)
+  /** Brand color value: Cinnamon Satin. */
+  private val cinnamonSatin = Color(0xFFCF6679)
 
-  /** Brand color value: Maire. */
-  private val maire = Color(0xFF1E1B16)
+  /** Brand color value: Flavescent. */
+  private val flavescent = Color(0xFFFFE08D)
 
-  /** Brand color value: Maroon2. */
-  private val maroon2 = Color(0xFF402D00)
+  /** Brand color value: Maize (Crayola). */
+  private val maizeCrayola = Color(0xFFEBC349)
 
-  /** Brand color value: Maroon4. */
-  private val maroon4 = Color(0xFF690005)
+  /** Brand color value: 	Turquoise Green. */
+  private val turquoiseGreen = Color(0xFFADCFAE)
 
-  /** Brand color value: Melon. */
-  private val melon = Color(0xFFFFB4AB)
+  /** Brand color value: 	Eerie Black. */
+  private val eerieBlack = Color(0xFF1C1B17)
 
-  /** Brand color value: Myrtle. */
-  private val myrtle = Color(0xFF1E361B)
+  /** Brand color value: White Coffee. */
+  private val whiteCoffee = Color(0xFFE8E1D9)
 
-  /** Brand color value: Pixie Green. */
-  private val pixieGreen = Color(0xFFB1CFA8)
-
-  /** Brand color value: Saffron. */
-  private val saffron = Color(0xFFFABC27)
-
-  /** Brand color value: Spring Wood. */
-  private val springWood = Color(0xFFE9E1D9)
-
-  /** Brand color value: Stark White. */
-  private val starkWhite = Color(0xFFD1C5B4)
-
-  /** Brand color value: Wheat. */
-  private val wheat = Color(0xFFF6E0BB)
-
+  /** Wear colors palette. */
   val wearColorPalette: Colors = Colors(
-    primary = saffron,
-    primaryVariant = capeHoney,
-    onPrimary = maroon2,
 
-    secondary = pixieGreen,
-    secondaryVariant = wheat,
-    onSecondary = myrtle,
+    primary = flavescent,
+    primaryVariant = maizeCrayola,
+    onPrimary = Color.Black,
 
-    error = melon,
-    onError = maroon4,
+    secondary = turquoiseGreen,
+    secondaryVariant = turquoiseGreen,
+    onSecondary = Color.Black,
 
-    background = maire,
-    onBackground = springWood,
+    error = cinnamonSatin,
+    onError = Color.Black,
 
-    surface = maire,
-    onSurface = springWood,
-    onSurfaceVariant = starkWhite,
+    background = eerieBlack,
+    onBackground = whiteCoffee,
+
+    surface = eerieBlack,
+    onSurface = whiteCoffee
   )
 }
