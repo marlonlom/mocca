f5567bbb00eda55463f62e0dd6f7998d62161cb0
Marlon López <malm.gm@gmail.com>
2024-01-27 21:53:00 -0500

    feat(smartwatch-app): Updated calculation outputs for smartwatch

--- apps/wearos-app/src/main/kotlin/dev/marlonlom/apps/mocca/wearos/presentation/feats/calculator_output/CalculatorOutputs.kt
+++ apps/wearos-app/src/main/kotlin/dev/marlonlom/apps/mocca/wearos/presentation/feats/calculator_output/CalculatorOutputs.kt
@@ -6,41 +6,34 @@
 package dev.marlonlom.apps.mocca.wearos.presentation.feats.calculator_output
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Close
-import androidx.compose.material.icons.rounded.ErrorOutline
 import androidx.compose.material.icons.rounded.Refresh
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.ButtonDefaults
-import androidx.wear.compose.material.CardDefaults
 import androidx.wear.compose.material.CompactButton
 import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.TitleCard
 import dev.marlonlom.apps.mocca.calculator.Calculator
 import dev.marlonlom.apps.mocca.calculator.RequestedQuantity
 import dev.marlonlom.apps.mocca.calculator.model.CalculationException
-import dev.marlonlom.apps.mocca.calculator.model.CalculationResult
 import dev.marlonlom.apps.mocca.calculator.model.OrderResponse
 import dev.marlonlom.apps.mocca.wearos.R
-import kotlin.math.roundToLong
 
+/**
+ * Calculator output view composable ui.
+ *
+ * @author marlonlom
+ *
+ * @param amountText Input amount text value.
+ * @param onBackNavigationAction Action for back navigation.
+ */
 @Composable
 fun CalculatorOutput(
   amountText: String,
@@ -86,106 +79,16 @@ fun CalculatorOutput(
 
 }
 
+/**
+ * Shared Back navigation button composable ui for success and failure output.
+ *
+ * @author marlonlom
+ *
+ * @param isFailure True/False if the output result is failure.
+ * @param onBackNavigationAction Action for back navigation.
+ */
 @Composable
-private fun SuccessCalculatorOutput(
-  calculationResult: CalculationResult,
-  onBackNavigationAction: () -> Unit
-) {
-  Column(
-    modifier = Modifier
-      .padding(top = 40.dp, bottom = 20.dp)
-      .padding(horizontal = 20.dp)
-      .verticalScroll(rememberScrollState()),
-    verticalArrangement = Arrangement.spacedBy(10.dp),
-    horizontalAlignment = Alignment.CenterHorizontally
-  ) {
-    CalculationTitleCard(
-      titleRes = R.string.text_result_label_fee,
-      calculationResultValue = calculationResult.fixedFee.plus(calculationResult.variableFee)
-    )
-    CalculationTitleCard(
-      titleRes = R.string.text_result_label_total,
-      calculationResultValue = calculationResult.total
-    )
-    BackNavigationButton(
-      isFailure = false,
-      onBackNavigationAction = onBackNavigationAction
-    )
-  }
-}
-
-@Composable
-private fun CalculationTitleCard(
-  titleRes: Int,
-  calculationResultValue: Double
-) {
-  TitleCard(
-    onClick = {},
-    title = {
-      Text(
-        modifier = Modifier.fillMaxWidth(),
-        text = stringResource(titleRes),
-        maxLines = 1,
-        color = MaterialTheme.colors.secondary,
-        textAlign = TextAlign.Right,
-        fontWeight = FontWeight.Bold,
-        style = MaterialTheme.typography.caption3
-      )
-    },
-    backgroundPainter = CardDefaults.cardBackgroundPainter(
-      startBackgroundColor = MaterialTheme.colors.surface,
-      endBackgroundColor = MaterialTheme.colors.surface,
-    )
-  ) {
-    Text(
-      modifier = Modifier.fillMaxWidth(),
-      text = stringResource(
-        R.string.text_cop_currency_value,
-        calculationResultValue.roundToLong()
-      ),
-      maxLines = 1,
-      color = MaterialTheme.colors.secondary,
-      textAlign = TextAlign.Right,
-      style = MaterialTheme.typography.body2
-    )
-  }
-}
-
-@Composable
-private fun FailureCalculatorOutput(
-  alertMessageText: String,
-  onBackNavigationAction: () -> Unit
-) {
-  Column(
-    modifier = Modifier
-      .padding(top = 30.dp, bottom = 20.dp)
-      .padding(horizontal = 20.dp),
-    verticalArrangement = Arrangement.spacedBy(10.dp),
-    horizontalAlignment = Alignment.CenterHorizontally
-  ) {
-    Icon(
-      imageVector = Icons.Rounded.ErrorOutline,
-      tint = MaterialTheme.colors.error,
-      contentDescription = null
-    )
-    Text(
-      modifier = Modifier
-        .fillMaxWidth()
-        .padding(vertical = 10.dp),
-      text = alertMessageText,
-      color = MaterialTheme.colors.error,
-      textAlign = TextAlign.Center,
-      style = MaterialTheme.typography.caption2
-    )
-    BackNavigationButton(
-      isFailure = true,
-      onBackNavigationAction = onBackNavigationAction
-    )
-  }
-}
-
-@Composable
-private fun BackNavigationButton(
+internal fun BackNavigationButton(
   isFailure: Boolean,
   onBackNavigationAction: () -> Unit
 ) {
@@ -193,9 +96,12 @@ private fun BackNavigationButton(
     backgroundColor = if (isFailure) MaterialTheme.colors.error else MaterialTheme.colors.secondary,
     contentColor = if (isFailure) MaterialTheme.colors.onError else MaterialTheme.colors.onSecondary,
   )
-  CompactButton(colors = buttonColors, onClick = {
-    onBackNavigationAction()
-  }) {
+  CompactButton(
+    colors = buttonColors,
+    onClick = {
+      onBackNavigationAction()
+    },
+  ) {
     Icon(
       modifier = Modifier.size(DpSize(width = 20.dp, height = 20.dp)),
       imageVector = if (isFailure) Icons.Rounded.Close else Icons.Rounded.Refresh,
