0c3f84247fe762b49d4bbdf7f0f454cca493487c
Marlon López <malm.gm@gmail.com>
2024-03-20 21:22:32 -0500

    feat(mobile-app): Updated build gradle

--- apps/mobile-app/build.gradle.kts
+++ apps/mobile-app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
     buildConfig = true
   }
   composeOptions {
-    kotlinCompilerExtensionVersion = "1.5.8"
+    kotlinCompilerExtensionVersion = libs.versions.kotlinComposeCompiler.get()
   }
   packaging {
     resources {
