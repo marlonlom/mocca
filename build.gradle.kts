b7be8723a60a646af29dda261bb4a5c817cb423b
Marlon López <malm.gm@gmail.com>
2024-05-26 15:09:54 -0500

    fix: Adopted version 2.0.0 and k2 compiler

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,4 +23,5 @@ plugins {
   alias(libs.plugins.android.test) apply false
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.jvm) apply false
+  alias(libs.plugins.compose.compiler) apply false
 }
