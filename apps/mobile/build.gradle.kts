a058cb615b14151fb2dc1d27994365902a95fe5a
Marlon López <malm.gm@gmail.com>
2025-04-27 09:55:54 -0500

    Update build.gradle.kts

--- apps/mobile/build.gradle.kts
+++ apps/mobile/build.gradle.kts
@@ -84,7 +84,6 @@ dependencies {
   implementation(libs.androidx.compose.ui.tooling.preview)
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.core.splashscreen)
-  implementation(libs.androidx.datastore)
   implementation(libs.androidx.datastore.preferences)
   implementation(libs.androidx.lifecycle.runtime.compose)
   implementation(libs.androidx.lifecycle.runtime.ktx)
