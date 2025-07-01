a796bca04268f08779403136f713114485ce0b59
Marlon López <malm.gm@gmail.com>
2025-07-01 09:29:31 -0500

    build(core:ui): add compile options

--- features/core/ui/build.gradle.kts
+++ features/core/ui/build.gradle.kts
@@ -1,9 +1,11 @@
-import dev.marlonlom.mocca.configs.Config
-
 /*
  * Copyright 2024 Marlonlom
  * SPDX-License-Identifier: Apache-2.0
  */
+
+import dev.marlonlom.mocca.configs.Config
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
   id("com.android.library")
@@ -14,4 +16,15 @@ plugins {
 android {
   namespace = "dev.marlonlom.mocca.core.ui"
   compileSdk = Config.mobile.compileSdkVersion
+
+  compileOptions {
+    sourceCompatibility = Config.jvm.javaVersion
+    targetCompatibility = Config.jvm.javaVersion
+  }
+}
+
+tasks.withType<KotlinJvmCompile>().configureEach {
+  compilerOptions {
+    jvmTarget.set(JvmTarget.fromTarget(Config.jvm.kotlinJvm))
+  }
 }
