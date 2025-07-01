c71b6f8ed9ecafdc440ee094c87aac849443f250
Marlon López <malm.gm@gmail.com>
2025-07-01 09:10:59 -0500

    build(apps:wearos): apply convention plugin

--- apps/wearos/build.gradle.kts
+++ apps/wearos/build.gradle.kts
@@ -3,68 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-
 plugins {
-  alias(libs.plugins.android.application)
-  alias(libs.plugins.kotlin.android)
-  alias(libs.plugins.compose.compiler)
-}
-
-android {
-  namespace = "dev.marlonlom.mocca"
-  compileSdk = 35
-
-  defaultConfig {
-    applicationId = "dev.marlonlom.mocca"
-    minSdk = 30
-    targetSdk = 35
-    versionCode = 342101
-    versionName = "1.0.1"
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-    vectorDrawables {
-      useSupportLibrary = true
-    }
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = true
-      isShrinkResources = true
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro",
-      )
-    }
-    create("benchmark") {
-      initWith(buildTypes.getByName("release"))
-      signingConfig = signingConfigs.getByName("debug")
-      matchingFallbacks += listOf("release")
-      isDebuggable = false
-    }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-  }
-  buildFeatures {
-    compose = true
-    buildConfig = true
-  }
-  packaging {
-    resources {
-      excludes += "/META-INF/{AL2.0,LGPL2.1}"
-    }
-  }
-}
-
-tasks.withType<KotlinJvmCompile>().configureEach {
-  compilerOptions {
-    jvmTarget.set(JvmTarget.fromTarget(JavaVersion.VERSION_17.majorVersion))
-  }
+  id("mocca.android.app.wearos")
+  id("mocca.compose.app")
 }
 
 dependencies {
@@ -75,26 +16,10 @@ dependencies {
   implementation(composeBom)
   androidTestImplementation(composeBom)
 
-  implementation(libs.androidx.activity.compose)
-  implementation(libs.androidx.compose.material.icons.extended)
-  implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.ui.tooling)
-  implementation(libs.androidx.compose.ui.tooling.preview)
-  implementation(libs.androidx.core.ktx)
-  implementation(libs.androidx.core.splashscreen)
   implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.androidx.wear.compose.foundation)
   implementation(libs.androidx.wear.compose.material3)
   implementation(libs.androidx.wear.compose.navigation)
   implementation(libs.google.play.services.wearable)
-
-  testImplementation(libs.junit)
-
-  androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-  androidTestImplementation(libs.androidx.test.ext.junit)
-  androidTestImplementation(libs.androidx.test.espresso.core)
-  androidTestImplementation(libs.google.truth)
-
-  debugImplementation(libs.androidx.compose.ui.tooling)
-  debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
