295879ff0b2db09d5c9e5a4987ae92137708aacb
Marlon López <malm.gm@gmail.com>
2025-07-01 09:10:46 -0500

    build(apps:mobile): apply convention plugin

--- apps/mobile/build.gradle.kts
+++ apps/mobile/build.gradle.kts
@@ -3,65 +3,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-@file:Suppress("DSL_SCOPE_VIOLATION")
-
 plugins {
-  alias(libs.plugins.android.application)
-  alias(libs.plugins.kotlin.android)
+  id("mocca.android.app.mobile")
+  id("mocca.compose.app")
   id("com.google.android.gms.oss-licenses-plugin")
-  alias(libs.plugins.compose.compiler)
-}
-
-android {
-  namespace = "dev.marlonlom.mocca"
-  compileSdk = 35
-
-  defaultConfig {
-    applicationId = "dev.marlonlom.mocca"
-    minSdk = 24
-    //noinspection OldTargetApi
-    targetSdk = 35
-    versionCode = 341100
-    versionName = "1.0.0"
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    vectorDrawables {
-      useSupportLibrary = true
-    }
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = true
-      isShrinkResources = true
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro",
-      )
-    }
-    create("benchmark") {
-      initWith(buildTypes.getByName("release"))
-      signingConfig = signingConfigs.getByName("debug")
-      matchingFallbacks += listOf("release")
-      isDebuggable = false
-    }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-  }
-  kotlinOptions {
-    jvmTarget = JavaVersion.VERSION_17.majorVersion
-  }
-  buildFeatures {
-    compose = true
-    buildConfig = true
-  }
-  packaging {
-    resources {
-      excludes += "/META-INF/{AL2.0,LGPL2.1}"
-    }
-  }
 }
 
 dependencies {
@@ -72,18 +17,11 @@ dependencies {
   implementation(composeBom)
   androidTestImplementation(composeBom)
 
-  implementation(libs.androidx.activity.compose)
   implementation(libs.androidx.appcompat)
   implementation(libs.androidx.browser)
-  implementation(libs.androidx.compose.material.icons.extended)
   implementation(libs.androidx.compose.material3)
   implementation(libs.androidx.compose.material3.wsc)
-  implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.ui.googlefonts)
-  implementation(libs.androidx.compose.ui.graphics)
-  implementation(libs.androidx.compose.ui.tooling.preview)
-  implementation(libs.androidx.core.ktx)
-  implementation(libs.androidx.core.splashscreen)
   implementation(libs.androidx.datastore.preferences)
   implementation(libs.androidx.lifecycle.runtime.compose)
   implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -94,15 +32,5 @@ dependencies {
   implementation(libs.google.oss.licenses)
   implementation(libs.jakewharton.timber)
 
-  testImplementation(libs.junit)
-  testImplementation(libs.kotlinx.coroutines.test)
-
-  androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-  androidTestImplementation(libs.androidx.test.ext.junit)
-  androidTestImplementation(libs.androidx.test.espresso.core)
   androidTestImplementation(libs.androidx.window.testing)
-  androidTestImplementation(libs.google.truth)
-
-  debugImplementation(libs.androidx.compose.ui.tooling)
-  debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
