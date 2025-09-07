7e7b392bd3679c28a01d98e39b0df7cb6e2fd44a
Marlon López <malm.gm@gmail.com>
2025-09-07 03:17:09 -0500

    feat(apps:mobile): integrate onboarding screen, update onboarding flow and transition to home screen

--- apps/mobile/src/main/kotlin/dev/marlonlom/mocca/ui/main/MainContent.kt
+++ apps/mobile/src/main/kotlin/dev/marlonlom/mocca/ui/main/MainContent.kt
@@ -4,14 +4,16 @@
  */
 package dev.marlonlom.mocca.ui.main
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import dev.marlonlom.mocca.mobile.onboarding.OnboardingScreen
 import dev.marlonlom.mocca.mobile.ui.theme.MoccaTheme
 import dev.marlonlom.mocca.ui.main.scaffold.AppScaffold
 import dev.marlonlom.mocca.ui.util.WindowSizeInfo
@@ -26,38 +28,42 @@ import dev.marlonlom.mocca.ui.util.WindowSizeInfo
  */
 @ExperimentalMaterial3Api
 @Composable
-internal fun MainContent(mainUiState: MainUiState, windowSizeInfo: WindowSizeInfo, mainActions: MainActions) =
-  MoccaTheme(
-    darkTheme = mainUiState.shouldUseDarkTheme(),
-    dynamicColor = mainUiState.shouldUseDynamicTheming(),
-  ) {
-    when (mainUiState) {
-      is MainUiState.Success -> {
-        when {
-          mainUiState.userData.isOnboarding -> {
-            Box(
-              modifier = Modifier
-                .fillMaxSize()
-                .safeContentPadding(),
-              contentAlignment = Alignment.Center
-            ) {
-              Text("Welcome")
-            }
-          }
-
-          else -> {
-            AppScaffold(
-              windowSizeInfo = windowSizeInfo,
-              mainActions = mainActions,
-            )
+internal fun MainContent(
+  mainUiState: MainUiState,
+  onOnboarded: () -> Unit,
+  windowSizeInfo: WindowSizeInfo,
+  mainActions: MainActions,
+) = MoccaTheme(
+  darkTheme = mainUiState.shouldUseDarkTheme(),
+  dynamicColor = mainUiState.shouldUseDynamicTheming(),
+) {
+  when (mainUiState) {
+    is MainUiState.Success -> {
+      when {
+        mainUiState.userData.isOnboarding -> {
+          Box(
+            modifier = Modifier
+              .fillMaxSize()
+              .background(MaterialTheme.colorScheme.background)
+              .safeContentPadding(),
+            contentAlignment = Alignment.Center,
+          ) {
+            OnboardingScreen { onOnboarded() }
           }
         }
-      }
 
-      else -> {}
+        else -> {
+          AppScaffold(
+            windowSizeInfo = windowSizeInfo,
+            mainActions = mainActions,
+          )
+        }
+      }
     }
 
+    else -> {}
   }
+}
 
 /**
  * Main actions data class.
