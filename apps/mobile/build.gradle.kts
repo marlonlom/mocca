92063cdc1c54aa545a5da4366da3b82782d74ab4
Marlon López <malm.gm@gmail.com>
2025-10-25 21:53:31 -0500

    feat(apps:mobile): integrate mobile ui and settings feature modules

--- apps/mobile/build.gradle.kts
+++ apps/mobile/build.gradle.kts
@@ -13,9 +13,10 @@ dependencies {
 
   implementation(project(":features:core:calculator"))
   implementation(project(":features:core:preferences"))
-  implementation(project(":features:mobile:onboarding"))
   implementation(project(":features:mobile:calculator-input"))
   implementation(project(":features:mobile:calculator-output"))
+  implementation(project(":features:mobile:onboarding"))
+  implementation(project(":features:mobile:settings"))
   implementation(project(":features:mobile:ui"))
 
   val composeBom = platform(libs.androidx.compose.bom)
