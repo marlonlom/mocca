9fab345266e0258d8741b9c6c825424d2b2e28c2
Marlon López <malm.gm@gmail.com>
2025-12-13 20:41:26 -0500

    Update app version retrieval to use LocalContext

--- features/mobile/settings/src/main/kotlin/dev/marlonlom/mocca/mobile/settings/layout/PortraitSettings.kt
+++ features/mobile/settings/src/main/kotlin/dev/marlonlom/mocca/mobile/settings/layout/PortraitSettings.kt
@@ -28,6 +28,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalResources
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -183,7 +184,7 @@ internal fun PortraitSettings(
         )
         SettingLink(
           title = stringResource(R.string.text_app_version),
-          subtitle = GeneralUtil.getAppVersionText(LocalResources.current),
+          subtitle = GeneralUtil.getAppVersionText(LocalContext.current),
           icon = {
             Icon(
               imageVector = Icons.TwoTone.Info,
