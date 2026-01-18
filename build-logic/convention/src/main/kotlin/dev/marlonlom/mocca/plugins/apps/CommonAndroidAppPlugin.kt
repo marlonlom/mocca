1a81f4e1dbf2c6fa254d7ae632311bcc48340d99
Marlon López <malm.gm@gmail.com>
2026-01-18 09:35:24 -0500

    build(convention): refactor compile options and cleanup plugins

--- build-logic/convention/src/main/kotlin/dev/marlonlom/mocca/plugins/apps/CommonAndroidAppPlugin.kt
+++ build-logic/convention/src/main/kotlin/dev/marlonlom/mocca/plugins/apps/CommonAndroidAppPlugin.kt
@@ -24,7 +24,6 @@ class CommonAndroidAppPlugin : Plugin<Project> {
     with(project) {
       with(pluginManager) {
         apply("com.android.application")
-        apply("kotlin-android")
         apply("mocca.koin")
         apply("mocca.spotless")
       }
