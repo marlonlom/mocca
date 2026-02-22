e9ed42770445b09c99992f56d0de57d1fd19a9da
Marlon López <malm.gm@gmail.com>
2026-02-22 13:08:53 -0500

    build(apps:mobile): cleanup unused dependencies

--- apps/mobile/build.gradle.kts
+++ apps/mobile/build.gradle.kts
@@ -28,15 +28,11 @@ dependencies {
   implementation(libs.androidx.compose.material3)
   implementation(libs.androidx.compose.material3.wsc)
   implementation(libs.androidx.compose.ui.googlefonts)
-  implementation(libs.androidx.datastore.preferences)
   implementation(libs.androidx.lifecycle.runtime.compose)
   implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.androidx.lifecycle.viewmodel.compose)
-  implementation(libs.androidx.navigation.compose)
-  implementation(libs.androidx.navigation.runtime.ktx)
   implementation(libs.androidx.window)
   implementation(libs.google.oss.licenses)
-  implementation(libs.jakewharton.timber)
 
   androidTestImplementation(libs.androidx.window.testing)
 }
