66349f0ee4b14aafda0c95239bcfb88638a9428e
Marlon López <malm.gm@gmail.com>
2026-03-08 21:56:00 -0500

    spotless(core:database): fix format violations

--- features/core/database/src/test/kotlin/dev/marlonlom/mocca/core/database/converters/DateConvertersTest.kt
+++ features/core/database/src/test/kotlin/dev/marlonlom/mocca/core/database/converters/DateConvertersTest.kt
@@ -1,8 +1,7 @@
 /*
- * Copyright 2026 Marlonlom
+ * Copyright 2024 Marlonlom
  * SPDX-License-Identifier: Apache-2.0
  */
-
 package dev.marlonlom.mocca.core.database.converters
 
 import org.junit.Assert.assertEquals
