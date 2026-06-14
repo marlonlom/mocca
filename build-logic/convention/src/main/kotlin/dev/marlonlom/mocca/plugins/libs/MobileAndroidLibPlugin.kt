2093456dd4cc1d7fde07e992d33e960a4b224ed9
Marlon López <malm.gm@gmail.com>
2026-06-14 16:26:57 -0500

    build(convention): add Material3 and Adaptive dependencies to MobileAndroidLibPlugin

--- build-logic/convention/src/main/kotlin/dev/marlonlom/mocca/plugins/libs/MobileAndroidLibPlugin.kt
+++ build-logic/convention/src/main/kotlin/dev/marlonlom/mocca/plugins/libs/MobileAndroidLibPlugin.kt
@@ -7,16 +7,22 @@ package dev.marlonlom.mocca.plugins.libs
 
 import com.android.build.api.dsl.LibraryExtension
 import dev.marlonlom.mocca.configs.Config
+import dev.marlonlom.mocca.extensions.versionCatalog
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
 class MobileAndroidLibPlugin : Plugin<Project> {
 
   override fun apply(project: Project) {
     with(project) {
       pluginManager.apply("mocca.android.lib.common")
-
+      dependencies {
+        add("implementation", versionCatalog().findLibrary("androidx-compose-material3").get())
+        add("implementation", versionCatalog().findLibrary("androidx-compose-material3-wsc").get())
+        add("implementation", versionCatalog().findBundle("m3-adaptive").get())
+      }
       extensions.configure<LibraryExtension> {
         namespace = Config.mobile.nameSpace
         compileSdk = Config.mobile.compileSdkVersion
